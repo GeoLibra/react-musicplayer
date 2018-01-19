4e24fab39dde80320a388a460c8b00d97bc19cc1
LuvJia <674530915@qq.com>
2018-01-19 21:54:07 +0800

    add less and less-loader through npm run eject

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import Header from './components/header';
 import './css/reset.css';
 import './css/common.css';
-import './css/index.less';
 
 import registerServiceWorker from './registerServiceWorker';
 
