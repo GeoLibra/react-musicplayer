e454064b10e26fd90fb3e4d84fbe92d219364039
LuvJia <674530915@qq.com>
2018-01-21 19:07:06 +0800

    add jplayer and jquery

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-
+import $ from 'jquery';
 import 'jplayer';
 import Progress from './progress';
 import Header from './header';
@@ -14,7 +14,7 @@ class Main extends Component{
         $('#player').jPlayer(
             {
                 ready:()=>{
-                    $(this).jPlayer(
+                    $('#player').jPlayer(
                         'setMedia',{
                             mp3:'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
                         }
